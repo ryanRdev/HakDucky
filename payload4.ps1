7f207312c62e19f5703a97ec3cb6c08bb855f88b
Ryan <ryanraymond2010@gmail.com>
2025-07-10 14:37:07 -0400

    Delete payload4.ps1

--- payload4.ps1
+++
@@ -1,7 +0,0 @@
-$sounds = Get-ChildItem "C:\Windows\Media" -Filter *.wav
-
-while ($true) {
-    $sound = Get-Random -InputObject $sounds
-    (New-Object Media.SoundPlayer $sound.FullName).PlaySync() 
-    Start-Sleep -Milliseconds 100
-}
